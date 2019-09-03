19467148e2e5b14f1295aaec8f3b3e3fb04aeb96
rojoypira <rjpira@hawaii.edu>
2019-09-03 11:17:01 -1000

    added initial files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var myMenuButton = document.querySelector('.menu-button');
+    var myMainMenu = document.querySelector('.site-header .site-navigation');
+    // console.log(myMainMenu);
+    myMenuButton.addEventListener('click',function(){
+    // console.log("hello!")
+        myMainMenu.classList.toggle('expanded');
+    });
+});
